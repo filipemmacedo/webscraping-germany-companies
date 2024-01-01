c04dec2596fcd93b2df5d95be383d8e8b37d0eb2
Filipe Macedo <filipe.macedo@doutorfinancas.pt>
2024-01-01 19:55:14 +0000

    initial commit

---
+++ SQL/Market-Revenue-Germany.sql
@@ -0,0 +1,13 @@
+SELECT 
+    t1.Name,
+    CAST(t2.revenue_eur AS FLOAT) AS Revenue_EUR,
+    CAST(t1.market_cap_eur AS FLOAT) AS Market_Cap_EUR,
+    CAST(t2.revenue_usd AS FLOAT) AS Revenue_USD,
+    CAST(t1.market_cap_usd AS FLOAT) AS Market_Cap_USD,
+    ROUND(CAST(t2.revenue_eur AS FLOAT) / NULLIF(CAST(t1.market_cap_eur AS FLOAT), 0), 2) AS RATIO_EUR,
+    ROUND(CAST(t2.revenue_usd AS FLOAT) / NULLIF(CAST(t1.market_cap_usd AS FLOAT), 0), 2) AS RATIO_USD,
+    t2.Headquarters AS HQ,
+    t2.Industry
+FROM 
+    [filipedb].[dbo].[companies_marketcap] AS t1
+    JOIN [filipedb].[dbo].[companies_revenue] AS t2 ON t1.NAME = t2.NAME;
