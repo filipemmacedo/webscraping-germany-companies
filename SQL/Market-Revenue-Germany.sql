e2cc212380ad185a376e482a0f3038002192d8be
Filipe Macedo <filipe.macedo@doutorfinancas.pt>
2024-01-02 09:17:52 +0000

    ordered by ratio

--- SQL/Market-Revenue-Germany.sql
+++ SQL/Market-Revenue-Germany.sql
@@ -4,10 +4,10 @@ SELECT
     CAST(t1.market_cap_eur AS FLOAT) AS Market_Cap_EUR,
     CAST(t2.revenue_usd AS FLOAT) AS Revenue_USD,
     CAST(t1.market_cap_usd AS FLOAT) AS Market_Cap_USD,
-    ROUND(CAST(t2.revenue_eur AS FLOAT) / NULLIF(CAST(t1.market_cap_eur AS FLOAT), 0), 2) AS RATIO_EUR,
-    ROUND(CAST(t2.revenue_usd AS FLOAT) / NULLIF(CAST(t1.market_cap_usd AS FLOAT), 0), 2) AS RATIO_USD,
+    ROUND(CAST(t2.revenue_eur AS FLOAT) / NULLIF(CAST(t1.market_cap_eur AS FLOAT), 0), 2) AS RATIO,
     t2.Headquarters AS HQ,
     t2.Industry
 FROM 
     [filipedb].[dbo].[companies_marketcap] AS t1
-    JOIN [filipedb].[dbo].[companies_revenue] AS t2 ON t1.NAME = t2.NAME;
+    JOIN [filipedb].[dbo].[companies_revenue] AS t2 ON t1.NAME = t2.NAME
+ORDER BY RATIO DESC;
